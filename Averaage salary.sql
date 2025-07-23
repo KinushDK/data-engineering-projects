4e2437189186bd371ecbf01a09db5347a6c3fd77
Dennis Kinuthia Kibunja <160403198+KinushDK@users.noreply.github.com>
2025-07-23 23:16:16 +0300

    Delete Averaage salary.sql

--- Averaage salary.sql
+++
@@ -1,6 +0,0 @@
--- find employess whose salary is above the average salary for their department 
-select employee_id, name, salary, role from employees e  
-where 
-salary > (
-	select avg(salary) from employees
-		)
